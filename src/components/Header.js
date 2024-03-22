77b4d32f591637ce9a783f1f159d13259a620fd6
Travicimo <mrtravicimo@gmail.com>
2024-03-22 18:52:49 +0900

    create pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,58 @@
-import React from 'react';
+import React from "react";
+
+import Socials from "./Socials";
+import Logo from "../img/header/logo.svg";
+import MobileNav from "./MobileNav";
+
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <div>Header here</div>;
+  return (
+    <header
+      className="bg-pink-200 fixed w-full px-[30px]
+     lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex 
+     item-center"
+    >
+      <div className="flex flex-col lg:flex-row 
+      lg:items-center w-full justify-between">
+        {/* logo */}
+        <Link to={"/"} className="max-w-[200px]">
+          <img src={Logo} alt="" />
+        </Link>
+        {/* nav - initially hidden - show on desktop mode*/}
+        <nav className="hidden xl:flex gap-x-12
+        font-semibold">
+          <Link
+            to={"/"}
+            className="text-[#696c6d] hover:text-primary transition"
+          >
+            Home
+          </Link>
+          <Link
+            to={"/about"}
+            className="text-[#696c6d] hover:text-primary transition"
+          >
+            About
+          </Link>
+          <Link
+            to={"/portfolio"}
+            className="text-[#696c6d] hover:text-primary transition"
+          >
+            Portfolio
+          </Link>
+          <Link
+           to={'/contact'} className="text-[#696c6d] hover:text-primary transition"
+        >
+          Contact
+          </Link>
+        </nav>
+        {/* socials */}
+        <Socials />
+        {/* mobile nav */}
+        <MobileNav />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
-
